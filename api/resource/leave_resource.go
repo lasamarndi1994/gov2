f56e3ebc60d3025b04448fa4c2300fc415fcc460
Lasa Marndi <lasamarndi199@gmail.com>
2025-08-16 09:09:54 +0530

    Update the leave changes

--- api/resource/leave_resource.go
+++ api/resource/leave_resource.go
@@ -1,6 +1,8 @@
 package resource
 
 import (
+	"time"
+
 	"github.com/lasamarndi1994/gov2/api/models"
 	"github.com/lasamarndi1994/gov2/utility/response"
 )
@@ -10,11 +12,13 @@ type LeaveTypeResponse struct {
 }
 
 type LeaveResponse struct {
-	ID        uint   `json:"id"`
-	LeaveType string `json:"leave_type"`
-	FromDate  string `json:"from_date"`
-	ToDate    string `json:"to_date"`
-	Remarks   string `json:"remarks"`
+	ID             uint      `json:"id"`
+	LeaveType      string    `json:"leave_type"`
+	FromDate       time.Time `json:"from_date"`
+	ToDate         time.Time `json:"to_date"`
+	FromLeaveValue string    `json:"from_leave_value"`
+	ToLeaveValue   string    `json:"to_leave_value"`
+	Remarks        string    `json:"remarks"`
 }
 
 // Map from []models.Leave → []LeaveResponse
@@ -22,12 +26,14 @@ func FromLeaves(leaves []models.Leave) interface{} {
 	res := make([]LeaveResponse, 0, len(leaves))
 	for _, l := range leaves {
 		res = append(res, LeaveResponse{
-			ID:        l.Id,
-			LeaveType: l.LeaveType.Name,
-			Remarks:   l.Remarks,
+			ID:             l.Id,
+			LeaveType:      l.LeaveType.Name,
+			FromDate:       l.FromDate,
+			ToDate:         l.ToDate,
+			FromLeaveValue: l.FromLeaveValue,
+			ToLeaveValue:   l.ToLeaveValue,
+			Remarks:        l.Remarks,
 		})
 	}
 	return response.SuccessResponse(res)
-
-	//return respose.
 }
